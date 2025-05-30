360493b19126242ccc90055a5e9326731c49e4a6
allanswifisucks <jl7550177@gmail.com>
2025-05-29 20:42:59 -0400

    new update

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ let dinos=document.getElementsByClassName("dino");
 let gameArea=document.getElementsByClassName("dinogame");
 let easy=document.getElementsByClassName("button1");
 let hard=document.getElementsByClassName("button2");
-let dinocoin=document.getElementsByClassName("dinocoin");
+let dinocoin=document.getElementsByClassName("dinocointext")[0];
+//game starts points = 0
+let points=0;
 let dinogame = gameArea[0];
 let dinospeed = 10;
 let dino = dinos[0];
 let spawn = 2700;
 let spawntimer;
+
+function easybutton(){
+  dinospeed = 10;
+}
+function hardbutton(){
+  dinospeed = 60;
+}
+
+function incscore(){
+  points++; 
+  // console.log(points);
+  dinocoin.textContent = 'Your score: ' + points;
+}
 // this function makes the dino jump
 document.addEventListener('keydown', function(e) {
     if (e.code === 'Space') {
@@ -62,26 +77,47 @@ function createobstacle(){
 
 
 function startMovingObstacles() {
+
   gameTimer = setInterval(function () {
+    // incscore();
     moveObstacles();  
     detectlose();
+    removeOffscreenObstacles()
+    // incscore();
   }, 20);
 }
 
 function moveObstacles(){
+  
   for (var i = 0; i < obstacles.length; i++) {
       var obs       = obstacles[i];                 // pick one obstacle
       var currentX  = parseInt(obs.style.left, 10); // where is obstacle at the moment
       obs.style.left = (currentX - dinospeed) + 'px'; // shift obstacle to the left
+     
   }
 }
 
 
 // Begin the loop that spawns new obstacles at spawnRate (declared above)
 function startSpawningObstacles() {
+  
   spawntimer = setInterval(function() {
     createobstacle();
+
   }, spawn);
+}
+/* Whenever an obstacle scrolls off the left edge, we remove it and reward the player with a point. */
+function removeOffscreenObstacles() {
+  for (var i = obstacles.length - 1; i >= 0; i--) {
+    var obs = obstacles[i];
+    var x   = parseInt(obs.style.left, 10);
+
+    if (x + obs.offsetWidth < 0) {       // obstacle is totally off-screen
+      dinogame.removeChild(obs);         
+      obstacles.splice(i, 1); 
+      incscore()
+    }
+  }
 }
 
 
